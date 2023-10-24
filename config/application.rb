71440291f33a0130f9a3b8e503cb6be3e06bf9e1
amtxantony <antony@amt-x.com.au>
2023-10-24 12:03:15 +1100

    cron

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Amtxapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+
+    config.hosts << "admin1.amtxlogistics.com.au"
   end
 end
